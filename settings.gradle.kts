3cfc5652a4e948dee027bf695c34ada8684eac9a
arngrame <arngrame.dev@gmail.com>
2025-04-26 14:18:08 +0400

    trigger build 2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,7 @@ buildCache {
         isAllowInsecureProtocol = true
         isAllowUntrustedServer = true
         isPush = System.getenv("GRADLE_REMOTE_CACHE_PUSH").toBoolean()
+
         credentials {
             username = System.getenv("GRADLE_REMOTE_CACHE_USERNAME")
             password = System.getenv("GRADLE_REMOTE_CACHE_PASSWORD")
