b3ac9e27f105cbcdd258ae8df93fd05d06f46b2c
arngrame <arngrame.dev@gmail.com>
2025-04-26 14:56:33 +0400

    trigger build 5

--- src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
+++ src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
@@ -89,40 +89,4 @@ class CalcControllerTest {
         assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
         assertThat(output.getBody()).isEqualTo(expected);
     }
-
-    @Test
-    public void whenFakeDuplicate() {
-        var input = new TwoArgs(-0, 0);
-        var expected = new Result(0);
-        var output = new CalcController().summarise(input);
-        assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
-        assertThat(output.getBody()).isEqualTo(expected);
-    }
-
-    @Test
-    public void whenFakeDuplicate2() {
-        var input = new TwoArgs(-0, 0);
-        var expected = new Result(0);
-        var output = new CalcController().summarise(input);
-        assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
-        assertThat(output.getBody()).isEqualTo(expected);
-    }
-
-    @Test
-    public void whenFakeDuplicate3() {
-        var input = new TwoArgs(-0, 0);
-        var expected = new Result(0);
-        var output = new CalcController().summarise(input);
-        assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
-        assertThat(output.getBody()).isEqualTo(expected);
-    }
-
-    @Test
-    public void whenFakeDuplicate4() {
-        var input = new TwoArgs(-0, 0);
-        var expected = new Result(0);
-        var output = new CalcController().summarise(input);
-        assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
-        assertThat(output.getBody()).isEqualTo(expected);
-    }
 }
