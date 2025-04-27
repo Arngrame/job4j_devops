e101bf4be00382731897e70dcf3d8ea823a95c60
arngrame <arngrame.dev@gmail.com>
2025-04-27 23:00:46 +0400

    tmp fix

--- src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
+++ src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
@@ -80,14 +80,4 @@ class CalcControllerTest {
         assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
         assertThat(output.getBody()).isEqualTo(expected);
     }
-
-    @Test
-    public void whenBothZeroNumbers555() {
-        var input = new TwoArgs(-0, 0);
-        var expected = new Result(0);
-        var output = new CalcController().summarise(input);
-        assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
-        assertThat(output.getBody()).isEqualTo(expected);
-    }
-
 }
